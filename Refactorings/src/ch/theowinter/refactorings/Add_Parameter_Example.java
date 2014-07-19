79607058338735354e5d0116f1ab0088672b2d8b
aerobless <theo.winter@gmail.com>
2014-07-19 10:58:10 +0200

    added first refactoring: Add Parameter

---
+++ Refactorings/src/ch/theowinter/refactorings/Add_Parameter_Example.java
@@ -0,0 +1,24 @@
+package ch.theowinter.refactorings;
+
+public class Add_Parameter_Example {
+	
+	/* 
+	 * Name: Add Parameter
+	 * Inverse refactoring: Remove Parameter
+	 * Description: A method requires more information from its caller.
+	 * 
+	 * Eclipse Refactoring Option: YES
+	 * Name: Change Method Signature
+	 */
+	
+	//BEFORE REFACTORING:
+	public void someMethod(){
+		System.out.println("do something");
+	}
+	
+	//AFTER REFACTORING:
+	public void someMethodWithParameter(String parameter){
+		System.out.println("do something with"+ parameter);
+	}
+
+}
